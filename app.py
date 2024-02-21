040fcd8e27b42103753f5aaaf69f047541bb9f99
kentkatigbak <102440753+kentkatigbak@users.noreply.github.com>
2024-02-21 15:57:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.markdown("""
 
 # Landing Page
 st.markdown("<p class='app_subtitle'>MANUFACTURING ENGINEERING DEPARTMENT | SYSTEMS ENGINEERING</p>", unsafe_allow_html=True)
-st.markdown("<p class='app_title'>WELCOME TO FMEA ONLINE!</p>", unsafe_allow_html=True)
+st.markdown("<p class='app_title'>FMEA ONLINE!</p>", unsafe_allow_html=True)
 
 
 # App key --- FMEA-SE24
